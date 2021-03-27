a7cfe4765b438eb971eea7e9bcfc2564f6a88972
Dan Duta <danduta23@gmail.com>
2021-03-27 16:46:01 +0200

    fixed fseek, ftell, added support for feof, ferr, improved fread, fwrite

--- 2-stdio/src/so_stdio.c
+++ 2-stdio/src/so_stdio.c
@@ -7,307 +7,373 @@
 
 #define BUFSIZE 4096
 
-#define READ    (1)
-#define WRITE   (1 << 1)
-#define APPEND  (1 << 2)
-#define UPDATE  (1 << 3)
+#define READ (1)
+#define WRITE (1 << 1)
+#define APPEND (1 << 2)
+#define UPDATE (1 << 3)
 
-typedef struct _so_file {
-    int fd;
-    unsigned short mode;
-    unsigned short last_op;
-    
-    char buffer[BUFSIZE];
-    size_t cursor;
-    int no_bytes;
+typedef struct _so_file
+{
+	int fd;
+	unsigned short mode;
+	unsigned short last_op;
+
+	char buffer[BUFSIZE];
+	size_t cursor;
+	int no_bytes;
+	int err;
+	int eof;
 } SO_FILE;
 
-SO_FILE *so_fopen(const char *pathname, const char *mode) {
-    if (!pathname || !mode)
-        return NULL;
+SO_FILE *so_fopen(const char *pathname, const char *mode)
+{
 
-    unsigned short fmode = 0;
-    int flags = 0;
-    if (!strcmp(mode, "r"))
-        fmode = READ;
-    else if (!strcmp(mode, "r+"))
-        fmode = READ | UPDATE;
-    else if (!strcmp(mode, "w"))
-        fmode = WRITE;
-    else if (!strcmp(mode, "w+"))
-        fmode = WRITE | UPDATE;
-    else if (!strcmp(mode, "a"))
-        fmode = APPEND;
-    else if (!strcmp(mode, "a+"))
-        fmode = APPEND | READ | UPDATE;
-    else
-        return NULL;
+	if (!pathname || !mode)
+		return NULL;
 
-    flags |= fmode & READ ? (fmode & UPDATE ? O_RDWR : O_RDONLY) : 0;
-    flags |= fmode & WRITE ? (fmode & UPDATE ? O_RDWR : O_WRONLY) | O_CREAT | O_TRUNC : 0;
-    flags |= fmode & APPEND ? (fmode & UPDATE ? O_RDWR : O_WRONLY) | O_APPEND | O_CREAT : 0;
+	unsigned short fmode = 0;
+	int flags = 0;
 
+	if (!strcmp(mode, "r"))
+		fmode = READ;
+	else if (!strcmp(mode, "r+"))
+		fmode = READ | UPDATE;
+	else if (!strcmp(mode, "w"))
+		fmode = WRITE;
+	else if (!strcmp(mode, "w+"))
+		fmode = WRITE | UPDATE;
+	else if (!strcmp(mode, "a"))
+		fmode = APPEND;
+	else if (!strcmp(mode, "a+"))
+		fmode = APPEND | READ | UPDATE;
+	else
+		return NULL;
 
-    int fd = open(pathname, flags, 0644);
-    if (fd < 0)
-        return NULL;
+	flags |= fmode & READ ? (fmode & UPDATE ? O_RDWR : O_RDONLY) : 0;
+	flags |= fmode & WRITE ? (fmode & UPDATE ? O_RDWR : O_WRONLY)
+			| O_CREAT | O_TRUNC : 0;
+	flags |= fmode & APPEND ? (fmode & UPDATE ? O_RDWR : O_WRONLY)
+			| O_APPEND | O_CREAT : 0;
 
-    SO_FILE* res = calloc(1, sizeof(SO_FILE));
-    if (!res) {
-        close(fd);
-        return NULL;
-    }
+	int fd = open(pathname, flags, 0644);
 
-    res->fd = fd;
-    res->mode = fmode;
+	if (fd < 0)
+		return NULL;
 
-    return res;
+	SO_FILE *res = calloc(1, sizeof(SO_FILE));
+
+	if (!res) {
+		close(fd);
+		return NULL;
+	}
+
+	res->fd = fd;
+	res->mode = fmode;
+
+	return res;
 }
 
-int so_fclose(SO_FILE *stream) {
-    if (!stream)
-        return SO_EOF;
-    
-    int rc;
+int so_fclose(SO_FILE *stream)
+{
+	if (!stream)
+		return SO_EOF;
 
-    if (so_fflush(stream) < 0)
-        return SO_EOF;
+	int rc;
 
-    rc = close(stream->fd);
-    if (rc < 0)
-        return SO_EOF;
+	if (so_fflush(stream) < 0) {
+		free(stream);
+		return SO_EOF;
+	}
 
-    free(stream);
+	rc = close(stream->fd);
+	if (rc < 0) {
+		free(stream);
+		return SO_EOF;
+	}
 
-    return 0;
+	free(stream);
+
+	return 0;
 }
 
-int so_fileno(SO_FILE *stream) {
-    if (!stream)
-        return -1;
+int so_fileno(SO_FILE *stream)
+{
+	if (!stream)
+		return -1;
 
-    return stream->fd;
+	return stream->fd;
 }
 
-int so_fflush(SO_FILE *stream) {
-    if (!stream)
-        return SO_EOF;
+int so_fflush(SO_FILE *stream)
+{
+	if (!stream)
+		return SO_EOF;
 
-    if (stream->last_op == READ) {
-        lseek(stream->fd, -stream->no_bytes, SEEK_CUR);
+	if (stream->last_op == READ) {
+		lseek(stream->fd, -stream->no_bytes, SEEK_CUR);
 
-        stream->cursor = 0;
-        stream->no_bytes = 0;
-        memset(stream->buffer, 0, BUFSIZE);
+		stream->cursor = 0;
+		stream->no_bytes = 0;
+		memset(stream->buffer, 0, BUFSIZE);
 
-        return 0;
-    }
+		return 0;
+	}
 
-    if (!stream->mode & (WRITE | APPEND))
-        return SO_EOF;
+	if (!stream->mode & (WRITE | APPEND | UPDATE))
+		return SO_EOF;
 
-    if (stream->last_op != WRITE)
-        return 0;
-    
-    if (stream->mode & APPEND)
-        so_fseek(stream, 0, SEEK_END);
+	if (stream->last_op != WRITE)
+		return 0;
 
-    int rc = write(stream->fd, stream->buffer, stream->cursor);
-    if (rc < 0)
-        return SO_EOF;
+	if (stream->mode & APPEND)
+		so_fseek(stream, 0, SEEK_END);
 
-    memset(stream->buffer, 0, BUFSIZE);
-    stream->cursor = 0;
+	int rc = write(stream->fd, stream->buffer, stream->cursor);
 
-    return 0;
+	if (rc < 0)
+		return SO_EOF;
+
+	memset(stream->buffer, 0, BUFSIZE);
+	stream->cursor = 0;
+
+	return 0;
 }
 
-int so_fseek(SO_FILE *stream, long offset, int whence) {
-    if (!stream)
-        return -1;
+int so_fseek(SO_FILE *stream, long offset, int whence)
+{
+	if (!stream)
+		return -1;
 
-    if (stream->last_op == READ) {
-        long current = so_ftell(stream);
+	if (stream->last_op == READ) {
+		long current = so_ftell(stream);
+		int dif, len;
 
-        int dif, len;
-        switch (whence) {
-            case SEEK_SET:
-                dif = offset - current;
-                break;
-            case SEEK_CUR:
-                dif = offset;
-                break;
-            case SEEK_END:
-                len = lseek(stream->fd, 0, SEEK_END);
-                if (len < 0)
-                    return -1;
+		switch (whence) {
+		case SEEK_SET:
+			dif = offset - current;
+			break;
+		case SEEK_CUR:
+			dif = offset;
+			break;
+		case SEEK_END:
+			len = lseek(stream->fd, 0, SEEK_END);
+			if (len < 0)
+				return -1;
 
-                if (lseek(stream->fd, current, SEEK_SET) < 0)
-                    return -1;
+			if (lseek(stream->fd, current, SEEK_SET) < 0)
+				return -1;
 
-                dif = len + offset - current;
-                break;
+			dif = len + offset - current;
+			break;
+		default:
+			return -1;
+		}
 
-            default:
-                return -1;
-        }
+		if (stream->cursor + dif >= 0 &&
+			stream->cursor + dif < stream->no_bytes) {
+			stream->cursor += dif;
+			return 0;
+		}
 
-        if (stream->cursor + dif >= 0 && stream->cursor + dif < stream->no_bytes) {
-            stream->cursor += dif;
-            return 0;
-        }
+		so_fflush(stream);
+	} else if (!(stream->mode & APPEND) && stream->last_op == WRITE &&
+				stream->cursor > 0) {
+		int rc = write(stream->fd, stream->buffer, stream->cursor);
 
-        so_fflush(stream);
-    } else if (stream->last_op == WRITE && stream->cursor > 0) {
-        int rc = write(stream->fd, stream->buffer, stream->cursor);
-        if (rc < 0)
-            return -1;
+		if (rc < 0)
+			return -1;
 
-        memset(stream->buffer, 0, BUFSIZE);
-        stream->cursor = 0;
-    }
+		memset(stream->buffer, 0, BUFSIZE);
+		stream->cursor = 0;
+	}
 
-    if (lseek(stream->fd, offset, whence) < 0)
-        return -1;
+	stream->last_op = 0;
+	if (lseek(stream->fd, offset, whence) < 0)
+		return -1;
 
-    return 0;
+	return 0;
 }
 
-long so_ftell(SO_FILE *stream) {
-    if (!stream)
-        return -1;
+long so_ftell(SO_FILE *stream)
+{
+	if (!stream)
+		return -1;
 
-    if (stream->last_op == READ)
-        return lseek(stream->fd, 0, SEEK_CUR) - (BUFSIZE - stream->cursor);
+	if (stream->last_op == READ) {
+		if (stream->no_bytes < BUFSIZE)
+			return stream->cursor;
 
-    return lseek(stream->fd, 0, SEEK_CUR) + stream->cursor;
+		return lseek(stream->fd, 0, SEEK_CUR) -
+				(BUFSIZE - stream->cursor);
+	}
+
+	return lseek(stream->fd, 0, SEEK_CUR) + stream->cursor;
 }
 
-int so_read_internal(SO_FILE *stream) {
-    if (!stream)
-        return SO_EOF;
+int so_read_internal(SO_FILE *stream)
+{
+	if (!stream)
+		return SO_EOF;
 
-    if (!(stream->mode & READ))
-        return SO_EOF;
+	if (!(stream->mode & READ))
+		return SO_EOF;
 
-    if (stream->cursor < stream->no_bytes)
-        return 0;
+	if (stream->cursor < stream->no_bytes)
+		return 0;
 
-    stream->no_bytes = read(stream->fd, stream->buffer, BUFSIZE);
-    if (stream->no_bytes <= 0)
-        return SO_EOF;
+	stream->no_bytes = read(stream->fd, stream->buffer, BUFSIZE);
+	if (stream->no_bytes <= 0) {
+		stream->eof = 1;
+		return SO_EOF;
+	}
 
-    stream->cursor = 0;
-    return stream->no_bytes;
+	stream->cursor = 0;
+	return stream->no_bytes;
 }
 
-int so_is_full_internal(SO_FILE *stream) {
-    if (!stream)
-        return 0;
+int so_is_full_internal(SO_FILE *stream)
+{
+	if (!stream)
+		return 0;
 
-    return stream->cursor > 0 && stream->cursor >= BUFSIZE;
+	return stream->cursor > 0 && stream->cursor >= BUFSIZE;
 }
 
-static inline int min(int a, int b) {
-    return a < b ? a : b;
+static inline int min(int a, int b)
+{
+	return a < b ? a : b;
 }
 
-size_t so_fread(void *ptr, size_t size, size_t nmemb, SO_FILE *stream) {
-    if (!ptr || size < 1 || nmemb < 1 || !stream)
-        return 0;
+size_t so_fread(void *ptr, size_t size, size_t nmemb, SO_FILE *stream)
+{
+	if (!ptr || size < 1 || nmemb < 1 || !stream)
+		return 0;
 
-    if (!(stream->mode & READ))
-        return 0;
+	if (!(stream->mode & READ))
+		return 0;
 
-    int total = size * nmemb;
-    int done = 0;
+	int total = size * nmemb;
+	int done = 0;
 
-    if (stream->last_op == WRITE)
-        so_fflush(stream);
+	if (stream->last_op == WRITE)
+		so_fflush(stream);
 
-    while (done < total) {
-        if (so_read_internal(stream) < 0)
-            return 0;
+	while (done < total) {
+		if (so_read_internal(stream) == SO_EOF) {
+			if (done < total)
+				stream->err = 1;
 
-        int writeable = min(stream->no_bytes - stream->cursor, total - done);
-        memcpy(ptr + done, stream->buffer + stream->cursor, writeable);
+			return done;
+		}
 
-        done += writeable;
-        stream->cursor += writeable;
-    }
+		int writeable =
+			min(stream->no_bytes - stream->cursor, total - done);
+		memcpy(ptr + done, stream->buffer + stream->cursor, writeable);
 
-    stream->last_op = READ;
-    return nmemb;
+		done += writeable;
+		stream->cursor += writeable;
+	}
+
+	stream->last_op = READ;
+	return nmemb;
 }
 
-size_t so_fwrite(const void *ptr, size_t size, size_t nmemb, SO_FILE *stream) {
-    if (!ptr || size < 1 || nmemb < 1 || !stream)
-        return 0;
+size_t so_fwrite(const void *ptr, size_t size, size_t nmemb, SO_FILE *stream)
+{
+	if (!ptr || size < 1 || nmemb < 1 || !stream)
+		return 0;
 
-    if (!(stream->mode & (WRITE | APPEND)))
-        return 0;
+	if (!(stream->mode & (WRITE | APPEND | UPDATE)))
+		return 0;
 
-    int total = size * nmemb;
-    int done = 0;
+	int total = size * nmemb;
+	int done = 0;
 
-    if (stream->last_op == READ)
-        so_fflush(stream);
+	if (stream->last_op == READ)
+		so_fflush(stream);
 
-    while (done < total) {
-        if (so_fputc(*(int*)(ptr + done), stream) != *(int*)(ptr + done))
-            return 0;
+	while (done < total) {
+		int *p = (int *)(ptr + done);
 
-        done++;
-    }
+		if (so_fputc(*p, stream) != *p) {
+			stream->err = 1;
+			return 0;
+		}
 
-    stream->last_op = WRITE;
-    return nmemb;
+		done++;
+	}
+
+	stream->last_op = WRITE;
+	return nmemb;
 }
 
-int so_fgetc(SO_FILE *stream) {
-    if (!stream)
-        return SO_EOF;
+int so_fgetc(SO_FILE *stream)
+{
+	if (!stream)
+		return SO_EOF;
 
-    if (!(stream->mode & READ))
-        return SO_EOF;
+	if (!(stream->mode & READ))
+		return SO_EOF;
 
-    if (stream->last_op == WRITE)
-        so_fflush(stream);
+	if (stream->last_op == WRITE)
+		so_fflush(stream);
 
-    if (stream->cursor < stream->no_bytes)
-        return stream->buffer[stream->cursor++];
+	if (stream->cursor < stream->no_bytes)
+		return stream->buffer[stream->cursor++];
 
-    int rc;
-    if (rc = so_read_internal(stream) < 0)
-        return rc;
+	int rc = so_read_internal(stream);
 
-    stream->last_op = READ;
-    return stream->buffer[stream->cursor++];
+	if (rc < 0)
+		return rc;
+
+	stream->last_op = READ;
+	return stream->buffer[stream->cursor++];
 }
 
-int so_fputc(int c, SO_FILE *stream) {
-    if (!stream)
-        return SO_EOF;
+int so_fputc(int c, SO_FILE *stream)
+{
+	if (!stream)
+		return SO_EOF;
 
-    if (!(stream->mode & (WRITE | APPEND)))
-        return SO_EOF;
+	if (!(stream->mode & (WRITE | APPEND | UPDATE)))
+		return SO_EOF;
 
-    if (stream->last_op == READ)
-        so_fflush(stream);
+	if (stream->last_op == READ)
+		so_fflush(stream);
 
-    if (so_is_full_internal(stream))
-        so_fflush(stream);
+	if (so_is_full_internal(stream)) {
+		int rc = so_fflush(stream);
 
-    stream->buffer[stream->cursor++] = c;
+		if (rc < 0) {
+			stream->err = 1;
+			return SO_EOF;
+		}
+	}
 
-    stream->last_op = WRITE;
+	stream->buffer[stream->cursor++] = c;
 
-    return c;
+	stream->last_op = WRITE;
 
+	return c;
 }
 
-int so_feof(SO_FILE *stream) {}
-int so_ferror(SO_FILE *stream) {}
+int so_feof(SO_FILE *stream)
+{
+	int current = so_ftell(stream);
+	int len = lseek(stream->fd, 0, SEEK_END);
+
+	if (lseek(stream->fd, current, SEEK_SET) < 0)
+		return -1;
+
+	return (current == len && stream->no_bytes == 0) || stream->eof ? 1 : 0;
+}
+int so_ferror(SO_FILE *stream)
+{
+	if (!stream)
+		return SO_EOF;
+
+	return stream->err;
+}
 
 SO_FILE *so_popen(const char *command, const char *type) {}
 int so_pclose(SO_FILE *stream) {}
